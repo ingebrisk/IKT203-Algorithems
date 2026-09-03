2714e50ec0a47155b7442f14a3c25f330a67f86d
Ingeborg <ingeborg.t.briskodden@gmail.com>
2026-09-03 15:48:25 +0200

    doing some changes

--- Submission/Submission 1/Step1-CppTemplateMastery.h
+++ Submission/Submission 1/Step1-CppTemplateMastery.h
@@ -1,11 +1,32 @@
 #pragma once
 #include <iostream>
+#include <tuple>
 using namespace std;
 
 
 /* -----------------------------------------------------
                 Step 1: C++ Template Mastery
    -----------------------------------------------------     */
+
+// 1. -------- Basic Template Functions -------------
+
+// a)
+template <typename T>
+void PrintTypeSize(T v){
+    cout << v << ", is containing " << sizeof(v) << " bytes" << endl;
+}
+
+// b)
+template <typename T>
+T GetMax(T a, T b) {
+    // make an if loops that checks for the biggest value between two values
+    if (a > b){
+        return a;
+    }else {
+        // we use else instead of else if because if a is not bigger then b, b will be the max value.         
+        return b;
+    } 
+}
 
 // c) the Twist
 struct TStudent{
@@ -28,4 +49,13 @@ public:
     bool operator>(const TStudent& other) const {
         return grade > other.grade;
     }
+};
+
+// 2. -------- Advanced Templates (Variadic and Non-Type) -------------
+
+// a) 
+
+template <typename... Args>
+struct TDataPack{
+    tuple<Args...> values;
 };
--- Submission/Submission 1/main.cpp
+++ Submission/Submission 1/main.cpp
@@ -1,7 +1,6 @@
 // Submission 1: Master of Templates and Callbacks
 #include <iostream>
-#include "TemplateFunction.h"
-#include "TStudent.h"
+#include "Step1-CppTemplateMastery.h"
 
 using namespace std;
 
@@ -10,11 +9,10 @@ using namespace std;
                 Step 1: C++ Template Mastery
    -----------------------------------------------------    
         1: BASIC TEMPLATE FUNCTIONS 
-            // a) and b) is in TemplateFunction.h
-            // c) is in TStudent.h 
+            // a) b) and c) in Step1-CppTemplateMastery.h
         
         2: ADVANCED TEMPLATES (Variadic and Non-Type)   
-            // a)  */
+            // a) b) and c) in Step1-CppTemplateMastery.h
 
 /* ------------------------------------------------------------------
                 Step 2: Global Callbacks (No Templates)
@@ -23,21 +21,21 @@ using namespace std;
 
 int main(){
     cout << " ---------------------------------------------- " << endl;
-    cout << "a) Function that printes the byte size of input: " << endl;
+    cout << "1. a) Function that printes the byte size of input: " << endl;
     cout << " ---------------------------------------------- " << endl;
     PrintTypeSize(453);  // integer
     PrintTypeSize('a');  // char Note: 'a' -> char, "a" -> string
     PrintTypeSize(3.14); // double
     cout << " --------------------------------------------- " << endl;
 
-    cout << "b) Functions that tell us biggest value between two values: " << endl;
+    cout << "1. b) Functions that tell us biggest value between two values: " << endl;
     cout << " --------------------------------------------- " << endl;
 
     GetMax(5,16);
     cout << GetMax(5,16) << ", is the biggest value" << endl;
     cout << " ---------------------------------------------- " << endl; 
 
-    cout << "c) The twist: " << endl;
+    cout << "1. c) The twist: " << endl;
     cout << " --------------------------------------------- " << endl;
     TStudent student1;
     student1.SetGrade(5);
@@ -45,7 +43,16 @@ int main(){
     student2.SetGrade(3);
     cout << GetMax(student1, student2).GetGrade() << ", is the highest grade" << endl;
     cout << " ---------------------------------------------- " << endl; 
+    cout << "2. a) Verdict Struct " << endl;
+    cout << " --------------------------------------------- " << endl;
+    TDataPack<int, string, double> pack;
+    pack.values = make_tuple(5, "hello", 3.14);
 
+    cout << get<0>(pack.values) << endl;
+    cout << get<1>(pack.values) << endl;
+    cout << get<2>(pack.values) << endl;
+    
+    cout << " --------------------------------------------- " << endl;
 
     return 0;
 }
